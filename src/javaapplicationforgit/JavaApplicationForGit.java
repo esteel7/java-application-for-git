2dc8af3dff61c767c9474dcf44121bc41604559f
Rodrigo Martinez <rcmartinezcampos@gmail.com>
2019-02-17 19:48:46 -0300

    Second commit

--- src/javaapplicationforgit/JavaApplicationForGit.java
+++ src/javaapplicationforgit/JavaApplicationForGit.java
@@ -1,18 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplicationforgit;
 
 /**
  *
- * @author Rodrigo
+ * @author Rodrigo Martínez
  */
 public class JavaApplicationForGit {
     
-    public static void main(String[] args) {
-        
-    }
     
+    public static void main(String[] args) {
+    
+        System.out.println("This is my first message");
+    }
 }
